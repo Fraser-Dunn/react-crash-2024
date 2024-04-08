fda0fbb63f41824a8da45d1778dc990ae0f2a688
Fraser Dunn <fraseradunn@gmail.com>
2024-04-08 17:36:43 +0100

    created router in app.js and begun moving componenets into a pages layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import VueAllJobs from "./components/VueAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <VueAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 export default App;
